990116c72cdca039f0904e2b64fa4b5be53704bd
長谷川勇太 <hasegawayuta@hasegawayuutanoMacBook-Pro.local>
2021-05-17 15:43:05 +0900

    add iterator

--- intelliJIDE/DesignPattern/src/iterator/BookShelf.kt
+++ intelliJIDE/DesignPattern/src/iterator/BookShelf.kt
@@ -1,4 +1,27 @@
-package iterator.iterator
+package iterator
 
-class BookShelf {
+/**
+ * 集合体クラス
+ *
+ * */
+class BookShelf(maxsize : Int): Aggregate{
+    private val books : Array<Book?> = arrayOfNulls(maxsize)
+
+    companion object{
+        private var last : Int = 0
+    }
+
+    val length : Int get() = last
+
+    // Bookの管理
+    fun getBookAt(index: Int): Book?{
+        return books[index]
+    }
+    fun appendBook(book: Book){
+        this.books[last] = book
+        last++
+    }
+    override fun iterator(): Iterator {
+        return BookShelfIterator(this)
+    }
 }
--- intelliJIDE/DesignPattern/src/iterator/BookShelfIterator.kt
+++ intelliJIDE/DesignPattern/src/iterator/BookShelfIterator.kt
@@ -1,4 +1,15 @@
-package iterator.iterator
+package iterator
 
-class BookShelfIterator {
+class BookShelfIterator(bookShelf : BookShelf) : Iterator{
+
+    private val mBookshelf: BookShelf = bookShelf
+    private var index: Int = 0
+
+    override fun hasNext(): Boolean = (index < mBookshelf.length)
+
+    override fun next(): Any {
+        val book = mBookshelf.getBookAt(index)
+        index++
+        return book as Any
+    }
 }
